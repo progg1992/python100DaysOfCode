d3828d8d59bc2815866486cbb3018fa2abe8327c
progg1992 <roggpierce@gmail.com>
2021-04-06 16:11:46 -0500

    Starter Blog from Day 67

--- Academic Projects/Days 60 to 69/Day_69/static/vendor/bootstrap/js/bootstrap.bundle.js
+++ Academic Projects/Days 60 to 69/Day_69/static/vendor/bootstrap/js/bootstrap.bundle.js
@@ -196,7 +196,7 @@
     triggerTransitionEnd: function triggerTransitionEnd(element) {
       $(element).trigger(TRANSITION_END);
     },
-    // TODO: Remove in v5
+
     supportsTransitionEnd: function supportsTransitionEnd() {
       return Boolean(TRANSITION_END);
     },
@@ -5092,7 +5092,6 @@
       }
     } // ----------------------------------------------------------------------
     // the following methods are used to handle overflowing modals
-    // todo (fat): these should probably be refactored out of modal.js
     // ----------------------------------------------------------------------
     ;
 
@@ -6373,7 +6372,6 @@
           var targetBCR = target.getBoundingClientRect();
 
           if (targetBCR.width || targetBCR.height) {
-            // TODO (fat): remove sketch reliance on jQuery position/offset
             return [$(target)[offsetMethod]().top + offsetBase, targetSelector];
           }
         }
